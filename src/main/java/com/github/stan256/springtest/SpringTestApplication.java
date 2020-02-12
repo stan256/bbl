60104253eee6eb6b3b4f74e6813e399ed9b351fe
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2020-02-12 23:02:21 +0100

    App properties

--- src/main/java/com/github/stan256/springtest/SpringTestApplication.java
+++ src/main/java/com/github/stan256/springtest/SpringTestApplication.java
@@ -2,8 +2,10 @@ package com.github.stan256.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringTestApplication {
 
     public static void main(String[] args) {
--- src/main/resources/db/migration/V1_2__Insert_basic_values.sql
+++ src/main/resources/db/migration/V1_2__Insert_basic_values.sql
@@ -3,3 +3,4 @@ INSERT INTO springtest.product_category (name) VALUES ('Jacket');
 INSERT INTO springtest.product_category (name) VALUES ('Laptop');
 INSERT INTO springtest.product_category (name) VALUES ('Jeans');
 INSERT INTO springtest.product_category (name) VALUES ('Watch');
+INSERT INTO springtest.product_category (name) VALUES ('Tablet');
