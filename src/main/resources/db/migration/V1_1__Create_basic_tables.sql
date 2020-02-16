cd595868619eb2fb64a1db30d1cdb9042bac57d4
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2020-02-16 19:28:03 +0100

    flyway & testcontainers sync

--- src/main/resources/db/migration/V1_1__Create_basic_tables.sql
+++ src/main/resources/db/migration/V1_1__Create_basic_tables.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS "booblschema";
-
 CREATE TABLE IF NOT EXISTS "user"
 (
     id           bigserial primary key,
--- src/test/java/com/github/stan256/bblaccount/ProductCategoryRepositoryTest.java
+++ src/test/java/com/github/stan256/bblaccount/ProductCategoryRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.github.stan256.bblaccount;
 
-import com.github.stan256.bblaccount.model.ProductCategory;
 import com.github.stan256.bblaccount.sql.BaseRepositoryTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
