1947e37af566a200008f3efa7e5a205e74151fcd
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2020-04-24 18:36:49 +0300

    step

--- src/test/java/com/github/stan256/bblaccount/controllers/AuthControllerIT.java
+++ src/test/java/com/github/stan256/bblaccount/controllers/AuthControllerIT.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AuthController.class)
+@WebMvcTest(value = AuthController.class, includeFilters = @ComponentScan.Filter(classes = EnableWebSecurity.class) )
 class AuthControllerIT {
+
+
     @Autowired
     private MockMvc mockMvc;
 
