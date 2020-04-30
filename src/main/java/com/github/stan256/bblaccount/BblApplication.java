54400b567637f5aefed3094408b4ebc91ca85ef5
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2020-04-30 18:06:40 +0300

    check

--- src/main/java/com/github/stan256/bblaccount/BblApplication.java
+++ src/main/java/com/github/stan256/bblaccount/BblApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // docker stop bbl & docker rm bbl & docker run -d -e POSTGRES_USER=bbl -e POSTGRES_PASSWORD=bbl --name bbl -p 1717:5432 postgres
 
+
 @SpringBootApplication
 public class BblApplication {
     public static void main(String[] args) {
